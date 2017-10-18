4295205f6a307105dbfe08a9475d0b114022734c
Flavins <maduabuchiokonkwo@gmail.com>
2017-10-18 23:05:32 +0100

    add eventListener and validation

--- percentageCalculator/main.js
+++ percentageCalculator/main.js
@@ -1,3 +1,20 @@
 var numField1 = document.getElementById("numField1");
 var numField2 = document.getElementById("numField1");
 var resultField = document.getElementById("resultField");
+var form = document.getElementById("xIsWhatPercentOfY");
+
+
+
+form.addEventListener("submit", function(){
+    if(!numField1.value || !numField2.value){
+        alert("please enter values in both fields");
+    }else{
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
+
+        alert(x + y);
+
+    }
+  
+
+});
